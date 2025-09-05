e0e45f22410924c0026949979ed5c7a5007bdbe0
Michael Harms <harmsm@protonmail.com>
2025-09-05 11:10:55 -0700

    docstring update

--- src/tfscreen/calibration/get_wt_theta.py
+++ src/tfscreen/calibration/get_wt_theta.py
@@ -5,7 +5,29 @@ def get_wt_theta(iptg,
                  calibration_data,
                  override_K=None,
                  override_n=None):
+    """
+    Get the slope of k vs. theta for combinations of marker and selection 
+    using the model stored in calibration_data.
+
+    Parameters
+    ----------
+    iptg : np.ndarray
+        1D array of iptg concentration for each condition
+    calibration_data : dict or str
+        a dictionary holding calibration values or the path to the calibration
+        json file
+    override_K : np.ndarray or float
+        use this K value for the hill model, not the K in calibration_data. if
+        an array, must match dimensions of iptg
+    override_n : np.ndarray or float
+        use this n value for the hill model, not the n in calibration_data. if
+        an array, must match dimensions of iptg
     
+    Returns
+    -------
+    np.ndarray
+        1D array of the slope of k vs. theta
+    """
     
     calibration_dict = read_calibration(calibration_data)
     
