450dc8872fe84c2d07fc3d2c9df547640fd97708
Michael Harms <harmsm@protonmail.com>
2025-09-05 10:26:35 -0700

    clean up calibration, add get_slope

--- src/tfscreen/calibration/__init__.py
+++ src/tfscreen/calibration/__init__.py
@@ -7,18 +7,22 @@ from .build_design_matrix import (
     build_design_matrix
 )
 
+from .get_wt_k import (
+    get_wt_k
+)
+
+from .get_wt_theta import (
+    get_wt_theta
+)
+
+from .get_slopes import (
+    get_slopes
+)
+
 from .build_calibration_dict import (
     build_calibration_dict
 )
 
 from .calibrate import (
     calibrate
-)
-
-from .get_wt_growth import (
-    get_wt_growth
-)
-
-from .get_wt_theta import (
-    get_wt_theta
 )
