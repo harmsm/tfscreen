5b6c9a59de92e960d52eacd3a84a9998150b3001
Michael Harms <harmsm@protonmail.com>
2026-09-22 09:20:13 -0700

    updated wipe.sh to catch stray editable install files

--- wipe.sh
+++ wipe.sh
@@ -1,4 +1,5 @@
 rm -rf ~/miniconda3/lib/python3.13/site-packages/tfscreen* 
 rm -rf ~/miniconda3/bin/tfs-*
 rm -rf build/ 
+rm -f ~/miniconda3/lib/python3.13/site-packages/__editable__.tfscreen-*.pth
 for x in `find . -iname "__pycache__"`; do rm -rf $x; done
